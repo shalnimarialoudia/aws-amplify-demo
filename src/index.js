666e08e01e413360c999a886cd8856f7ac8babc0
Your Name <you@example.com>
2023-02-27 22:26:49 +0530

    errror resolved

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {Amplify} from 'aws-amplify'
-import {awsmobile} from './aws-exports'
+import awsmobile from './aws-exports';
 
 Amplify.configure(awsmobile);
 const root = ReactDOM.createRoot(document.getElementById('root'));
